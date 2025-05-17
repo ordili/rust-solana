02924ff3a21de6f31d54fcdb91bd69dce2f4aa6a
ordili <qinqiqili@163.com>
2025-05-17 16:46:39 +0800

    add deser_option

--- src/serialize.rs
+++ src/serialize.rs
@@ -1,6 +1,9 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use std::collections::BTreeMap;
 
+use arrayref::{array_ref, array_refs};
+use solana_program::{program_option::COption, pubkey::Pubkey};
+
 #[derive(BorshDeserialize, BorshSerialize, Debug, PartialEq, Eq)]
 struct Primitive(
     u8,
@@ -12,9 +15,26 @@ struct Primitive(
     BTreeMap<String, String>,
 );
 
+/// Emulate how COption is 'unpacked'
+fn deser_option(data: &[u8]) -> COption<Pubkey> {
+    // Map the data block
+    let ain = array_ref![data, 0, 36];
+    let (base, key) = array_refs![ain, 4, 32];
+    // Get the SOME or NONE u32
+    let nos = u32::from_le_bytes(*base);
+    // Construct the COption accordingly
+    let opt: COption<Pubkey> = if nos == 0 {
+        COption::None
+    } else {
+        COption::Some(Pubkey::new_from_array(*key))
+    };
+    opt
+}
+
 mod test {
-    use crate::serialize::Primitive;
+    use crate::serialize::{Primitive, deser_option};
     use borsh::{BorshDeserialize, BorshSerialize};
+    use solana_sdk::pubkey::Pubkey;
     use std::collections::BTreeMap;
 
     #[test]
@@ -47,5 +67,24 @@ mod test {
         let origin_primitive = Primitive::try_from_slice(&buffer).unwrap();
 
         assert_eq!(origin_primitive, primitive);
+    }
+    #[test]
+    fn test_deser_option() {
+        // From Typescript with borsh'ing
+        let copt = [
+            1u8, 0, 0, 0, 135, 202, 71, 214, 68, 105, 98, 176, 211, 130, 105, 2, 55, 187, 86, 186,
+            109, 176, 80, 208, 77, 100, 221, 101, 20, 203, 149, 166, 96, 171, 119, 35,
+        ];
+        // Emulate COption deserialization
+        let coption = deser_option(&copt);
+        if coption.is_some() {
+            println!("{:?}", coption.expect("Uh-oh"));
+        }
+        // As a Borsh Struct
+        #[derive(BorshDeserialize, BorshSerialize, Debug)]
+        struct TOption(u32, [u8; 32]);
+        let toption = TOption::try_from_slice(&copt).unwrap();
+        let pkey = Pubkey::new_from_array(toption.1);
+        println!("Some = {:?} Pubkey = {:?}", toption.0, pkey);
     }
 }
